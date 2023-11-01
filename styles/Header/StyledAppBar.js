ee0b25612381bc7cb36a65c2aa0b1e57cd3ecb01
sivaShilpa <shilpa.vchand@gmail.com>
2023-11-01 11:34:28 -0400

    feat: initial setup - folder scaffolding

---
+++ styles/Header/StyledAppBar.js
@@ -0,0 +1,11 @@
+import {AppBar, styled} from '@mui/material';
+
+const StyledAppBar = styled(AppBar)({
+    display: 'flex',
+    justifyContent: 'space-between',
+    background: 'var(--dark-dark-2, #1F222A)',
+    height:'85px',
+    boxShadow: 'none',
+})
+
+export default StyledAppBar
---
+++ templates/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import StyledAppBar from '../../styles/Header/StyledAppBar'
+
+
+const Header = () => {
+
+  return (
+     <StyledAppBar>
+     Header        
+     </StyledAppBar>
+  )
+}
+
+export default Header
