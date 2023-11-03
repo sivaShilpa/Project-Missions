5a8b1ad36fd7ce74b7298560a75f296bfbcf0baa
sivaShilpa <shilpa.vchand@gmail.com>
2023-11-03 12:54:16 -0400

    fix: remove dead code in header/styles

--- styles/Header/styles.js
+++ styles/Header/styles.js
@@ -24,7 +24,6 @@ const headerStyles = {
     },
     appbarGridContainer: {
         container: true,
-        // xs: 'auto',
         width: '100%',
         height: '84px',
         display: 'flex',
